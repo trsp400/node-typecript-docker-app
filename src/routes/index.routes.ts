424d13500fe81da3b2fdf5c8529eb20f4328437a
Thiago Robles <roblesthiago3@gmail.com>
2021-04-24 19:08:15 -0300

    chore: include new routes

--- src/routes/index.routes.ts
+++ src/routes/index.routes.ts
@@ -1,11 +1,15 @@
-import {Router} from 'express';
-import { sessionsRoutes } from './sessions.routes';
+import { Router } from "express";
+import { sessionsRoutes } from "./sessions.routes";
 
-import {usersRoutes} from './users.routes';
+import { usersRoutes } from "./users.routes";
+import { addressesRoutes } from "./addresses.routes";
+import { healthRoutes } from "./health.routes";
 
 const router = Router();
 
-router.use('/users', usersRoutes)
-router.use('/sessions', sessionsRoutes)
+router.use("/users", usersRoutes);
+router.use("/addresses", addressesRoutes);
+router.use("/health", healthRoutes);
+router.use("/sessions", sessionsRoutes);
 
 export default router;
